5c6b2f85b0a9009ee21a5f04b7c4e9b7c4d88c75
Markus <45589963+MarkusBlackburn@users.noreply.github.com>
2024-10-15 10:41:03 +0300

    Delete src/environments directory

--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    apiUrl: 'https://dev.smarthome-team.store/api/',
-    clientId: '947828801124-8usg2d325e03v6ujig6rpjjtmcv32i1e.apps.googleusercontent.com'
-};
--- src/environments/environment.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-    apiUrl: 'https://dev.smarthome-team.store/api/'
-};
