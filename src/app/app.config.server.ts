284e41caaf80e9bbf023ffa661e0d1e53ca1dc13
Mykyta Prosin <Mykyta_Prosin@jabil.com>
2024-10-19 20:51:08 +0300

    added google auth flow

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,14 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { mergeApplicationConfig, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
+import { routes } from './app.routes';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withPreloading(PreloadAllModules))
   ]
 };
 
