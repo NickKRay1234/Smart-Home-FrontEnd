09ccb0e3bc901a184408821a53c1af13f5295973
VolodumurGruv <gru.v@i.ua>
2024-10-21 22:38:17 +0300

    after merged with master

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,33 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, PreloadAllModules, withPreloading, withComponentInputBinding } from '@angular/router';
+import {
+  provideRouter,
+  PreloadAllModules,
+  withPreloading,
+  withComponentInputBinding,
+} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { provideNgxWebstorage, withSessionStorage } from 'ngx-webstorage';
-import { provideOAuthClient } from 'angular-oauth2-oidc'
+// import { provideOAuthClient } from 'angular-oauth2-oidc'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-  provideRouter(routes, withPreloading(PreloadAllModules), withComponentInputBinding()),
-  provideClientHydration(),
-  provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
-  provideNgxWebstorage(withSessionStorage()),
-  provideOAuthClient(),
-  ]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(
+      routes,
+      withPreloading(PreloadAllModules),
+      withComponentInputBinding()
+    ),
+    provideClientHydration(),
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
+    provideNgxWebstorage(withSessionStorage()),
+    // provideOAuthClient(),
+  ],
 };
