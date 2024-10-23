67d3751519b99afc6fed932ac72b90bae3df55ef
VolodumurGruv <gru.v@i.ua>
2024-10-23 19:42:04 +0300

    keep working on carusel

--- src/app/components/products/product-card/product-card.component.ts
+++ src/app/components/products/product-card/product-card.component.ts
@@ -1,5 +1,6 @@
 import {
   Component,
+  HostBinding,
   input,
   InputSignal,
   OnChanges,
--- src/app/components/products/product-slider/product-slider.component.ts
+++ src/app/components/products/product-slider/product-slider.component.ts
@@ -1,12 +1,5 @@
 import { NgClass } from '@angular/common';
-import {
-  AfterViewChecked,
-  AfterViewInit,
-  ChangeDetectorRef,
-  Component,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { AdvSlider, advSlider } from '@shared/configs/adv-slider.config';
 import { PricePipe } from '@core/pipes/price.pipe';
@@ -18,43 +11,15 @@ import { PricePipe } from '@core/pipes/price.pipe';
   templateUrl: './product-slider.component.html',
   styleUrl: './product-slider.component.css',
 })
-export class ProductSliderComponent implements AfterViewInit, OnDestroy {
+export class ProductSliderComponent {
+  @Input() autoSlide = false;
+
   advProducts: AdvSlider[] = advSlider;
   advCurrentProduct: AdvSlider = this.advProducts[0];
   selectedIdx = 0;
-  interval = 3000;
-  private autoPlayInterval!: ReturnType<typeof setInterval>;
-
-  constructor(private changeRef: ChangeDetectorRef) {}
-
-  ngAfterViewInit(): void {
-    this.autoPlay();
-  }
 
   isActive(idx: number): void {
     this.selectedIdx = idx;
     this.advCurrentProduct = this.advProducts[idx];
-  }
-
-  private play(): void {
-    if (this.selectedIdx >= this.advProducts.length - 1) {
-      this.selectedIdx = 0;
-    } else {
-      this.selectedIdx++;
-    }
-    this.advCurrentProduct = this.advProducts[this.selectedIdx];
-  }
-
-  autoPlay(): void {
-    this.autoPlayInterval = setInterval(() => {
-      this.play();
-      clearInterval(this.autoPlayInterval);
-    }, this.interval);
-  }
-
-  ngOnDestroy(): void {
-    if (this.autoPlayInterval) {
-      clearInterval(this.autoPlayInterval);
-    }
   }
 }
