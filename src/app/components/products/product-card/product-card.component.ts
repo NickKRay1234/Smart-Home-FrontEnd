8e2b491ab9ee1352629b94bfd36e16e41a8bc606
VolodumurGruv <gru.v@i.ua>
2024-10-23 23:28:48 +0300

    added btn global styles

--- src/app/components/products/product-card/product-card.component.ts
+++ src/app/components/products/product-card/product-card.component.ts
@@ -1,6 +1,7 @@
 import {
   Component,
   HostBinding,
+  HostListener,
   input,
   InputSignal,
   OnChanges,
@@ -9,11 +10,12 @@ import {
 import { Product } from '@shared/models/product/product';
 import { CutStringPipe } from '../../../core/pipes/cut-string.pipe';
 import { SvgIconComponent } from '@shared/components/svg-icon/svg-icon.component';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CutStringPipe, SvgIconComponent],
+  imports: [CutStringPipe, SvgIconComponent, NgClass, NgIf],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
 })
@@ -23,9 +25,18 @@ export class ProductCardComponent implements OnChanges {
   end: InputSignal<number> = input.required();
   products: Product[] = [];
   currentImage = 0;
+  currentIdx: number | null = null;
   heart = '/assets/images/svg/heart.svg';
   arrowLeft = '/assets/images/svg/left-arrow.svg';
   arrowRight = '/assets/images/svg/right-arrow.svg';
+
+  onMouseEnter(i: number) {
+    this.currentIdx = i;
+  }
+
+  onMouseLeave(i: number) {
+    this.currentIdx = null;
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     this.products = this.productsInput()?.slice(this.start(), this.end());
