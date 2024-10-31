f7164a460729841f6d106c42bf2661d7c9d75f15
VolodumurGruv <gru.v@i.ua>
2024-10-31 02:31:19 +0200

    updated slider with products from back-end

--- src/app/shared/tools/random-adv.ts
+++ src/app/shared/tools/random-adv.ts
@@ -4,7 +4,7 @@ export const randomAdv = (products: Product[]): Product[] => {
   const randomProduct: Product[] = [];
   console.log(products);
   for (let i = 0; i < 3; i++) {
-    const random = Math.floor(Math.random() * 3);
+    const random = Math.floor(Math.random() * (products.length - 1));
     randomProduct.push(products[random]);
     console.log(random);
   }
