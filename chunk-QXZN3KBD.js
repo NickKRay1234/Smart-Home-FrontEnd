fcb8eb90753d154931a9e1f44c6faece961389df
Markus <gudvinrawson@gmail.com>
2024-11-13 21:12:32 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/NickKRay1234/Smart-Home-FrontEnd/commit/c28f94448e4162e51b8626357f0106771bd37649

--- chunk-QXZN3KBD.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-4CLCTAJ7.js";var t=[{path:"login",loadComponent:()=>import("./chunk-4G2CLWU6.js").then(o=>o.LoginComponent)},{path:"register",loadComponent:()=>import("./chunk-XRQGXR2P.js").then(o=>o.RegisterComponent)}];export{t as accountRoutes};
