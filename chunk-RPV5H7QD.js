4eba378a64291bfca67307065173eb0949051155
Markus <gudvinrawson@gmail.com>
2024-11-23 16:31:20 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/NickKRay1234/Smart-Home-FrontEnd/commit/74eae6dafb0de6dcf568a5badf6ad0d0628f4cbb

--- chunk-RPV5H7QD.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-4CLCTAJ7.js";var o=[{path:"",loadComponent:()=>import("./chunk-TUBYXXE2.js").then(t=>t.ProductsComponent)},{path:"",redirectTo:"/",pathMatch:"full"}];export{o as PRODUCT_ROUTING};
